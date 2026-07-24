df0af60120e891b069a629943ac6f80cf2dc00d7
Mohamed Diaa <mohameddiaaahdahed@gmail.com>
2026-07-24 16:49:15 +0300

    secound commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white sm:items-start">
-        QR SCANINGقارئ الباركود 
+      <main className="flex w-full max-w-3xl items-center justify-between py-32 px-16 bg-white sm:items-start">
+        مرحبا بك الى نظام الدعوات الالكترونى لخدمة السوق الخليجى
+        <Link href="/login">
+          <Button>تسجيل دخول</Button>
+        </Link>
       </main>
     </div>
   );
